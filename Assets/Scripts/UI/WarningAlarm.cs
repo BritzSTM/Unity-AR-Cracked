22e0407051664a75f75e9abf22b0c554c0800518
BritzSTM <ha40672@gmail.com>
2021-08-25 16:02:49 +0900

    Create 경고 UI는 이제 효과음을 가진다

--- Assets/Scripts/UI/WarningAlarm.cs
+++ Assets/Scripts/UI/WarningAlarm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(AudioSource))]
 public class WarningAlarm : MonoBehaviour
 {
     public bool ActiveAlarm = false;
@@ -10,11 +11,25 @@ public class WarningAlarm : MonoBehaviour
     public float BlinkTime = 1.0f;
     private float _accDeltaTime = 0.0f;
 
-    void Update()
+    [SerializeField] private AudioClip _alarmSoundFX;
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = _alarmSoundFX;
+        _audioSource.loop = true;
+
+        Debug.Assert(_audioSource != null && _alarmSoundFX != null);
+    }
+
+    private void Update()
     {
         if(!ActiveAlarm)
         {
             _alarmBG.color = Color.green;
+            _audioSource.Stop();
+
             return;
         }
 
@@ -26,5 +41,8 @@ public class WarningAlarm : MonoBehaviour
         _accDeltaTime += Time.deltaTime;
         if (_accDeltaTime > BlinkTime)
             _accDeltaTime = 0.0f;
+
+        if (!_audioSource.isPlaying)
+            _audioSource.Play();
     }
 }
